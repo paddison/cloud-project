aff93b38f5c18e6f14d379274d47301950f281b7
Antonia Dietz <ad099@hdm-stuttgart.de>
2022-05-19 16:46:42 +0200

    tried to implement sinus function

--- sine_generator/src/lib.rs
+++ sine_generator/src/lib.rs
@@ -4,10 +4,11 @@ use writer::WriteExtension;
 
 pub mod writer;
 /// A struct containing metadata about the Wave file that will be created.
+#[derive(Clone, Copy)]
 pub struct WavSpec {
-    number_of_channels: u16,
-    sample_rate: u32,
-    bits_per_sample: u16,
+    pub number_of_channels: u16,
+    pub sample_rate: u32,
+    pub bits_per_sample: u16,
 }
 
 impl WavSpec {
--- sine_generator/src/main.rs
+++ sine_generator/src/main.rs
@@ -1,26 +1,118 @@
 use std::f64::consts::PI;
+use std::io::{Write, Seek};
 use sine_generator::WavSpec;
 use sine_generator::writer::WavWriter;
 
-fn main() {
+// pub struct WavSpec {
+//     number_of_channels: u16,
+//     sample_rate: u32,
+//     bits_per_sample: u16,
+// }
 
-    // testing out the writer
-    let time = 60;
-    let root = 220;
-    let prim = write_frequency(root, 44100, time);
-    let third = write_frequency(root * 5 / 4, 44100, time);
-    let fifth = write_frequency(root * 3 / 2, 44100, time);
-    // let fifth
-    let spec = WavSpec::new(1, 44100, 8).expect("Invalid specification");
-    let mut writer = WavWriter::new_with_spec(spec, "test_8.wav").expect("Error creating file");
-    for ((prim, third), fifth) in prim.iter().zip(third.iter()).zip(fifth.iter()) {
-        let _ = writer.write_sample(((*prim as u16 + *third as u16 + *fifth as u16) as f64 * 1. / 3.) as u8); 
+struct SineWavSpec {
+    wav_spec: WavSpec,
+    frequencies: Vec<u16>,
+    duration: u16,
+    volume: f64,
+}
+
+impl SineWavSpec {
+    pub fn new(wav_spec: WavSpec, frequencies: Vec<u16>, duration: u16, volume: f64) -> Option<Self> {
+       //other/different conditions?
+        if frequencies.len() < 1 || duration < 1 {
+            None
+        } else {
+            Some(SineWavSpec { wav_spec, frequencies, duration, volume })
+        }
     }
 }
 
-fn write_frequency(freq: u32, sample_rate: usize, time: usize) -> Vec<u8> {
+fn write_wave<W>(sine_spec: SineWavSpec, mut wav_writer: WavWriter<W>) 
+where W: Seek + Write
+{
+    //what to do with volume?
+    //aws lambda integration?
+    let all_freqs = write_all_frequencies(&sine_spec);
+
+    if sine_spec.wav_spec.bits_per_sample == 16 {
+        for freq in all_freqs.iter() {
+            let _ = wav_writer.write_sample((freq * 1. / sine_spec.frequencies.len() as f64) as i16);
+        } 
+    } else {
+    
+        for freq in all_freqs.iter() {
+            println!("{}", (freq * 1. / sine_spec.frequencies.len() as f64) as u8);
+            //println!("{}", (freq * 1. / all_freqs.len() as f64) );
+            let _ = wav_writer.write_sample((freq * 1. / sine_spec.frequencies.len() as f64) as u8);
+        } 
+    }
+}
+
+fn write_all_frequencies(sine_spec: &SineWavSpec) -> Vec<f64> {
+    let mut all_freqs_u8 : Vec<Vec<u8>> = vec![];
+    let mut all_freqs_u16 : Vec<Vec<u16>> = vec![]; 
+    let mut data = vec![];
+    let x_values = sine_spec.wav_spec.sample_rate * sine_spec.duration as u32;
+    // calculate each datapoint indivdually
+    // add all together 
+    // normalize
+    // scale to bits per sample
+    // call write
+    for i in 0..x_values {
+        let mut data_point = 0.;
+        for freq in &sine_spec.frequencies {
+            data_point  += calculate_frequency(i, *freq, sine_spec.wav_spec.sample_rate)
+        }
+        data_point /= sine_spec.frequencies.len() as f64;
+        // scale to bits_per_sample (you can do it!!!!) 
+        // 8 bit are unsigned, 16 bit signed
+        // signed means can be negative
+        // negative means it can be less than 0
+        
+        // 0 is a number (see https://en.wikipedia.org/wiki/0 for reference)
+        data_point = match sine_spec.wav_spec.bits_per_sample {
+            8 => (data_point * 128.) as i16 + 127,
+            16 => (data_point * i16::MAX) as i16 + 127,
+            _ => 0.,
+        }
+
+        // return data point
+    }
+
+    if sine_spec.wav_spec.bits_per_sample == 16 {
+        for freq in sine_spec.frequencies.iter() {
+            all_freqs_u16.push(write_frequency_16(*freq, sine_spec.duration, sine_spec.wav_spec.sample_rate));
+        }
+        for i in 0..(sine_spec.duration as u32 * sine_spec.wav_spec.sample_rate) {
+            let mut freq_sum : f64 = 0.;
+            for freq in all_freqs_u16.iter() { freq_sum += freq[i as usize] as f64 }
+            data.push(freq_sum);
+            
+        }
+    } else {
+        for freq in sine_spec.frequencies.iter() {
+            all_freqs_u8.push(write_frequency_8(*freq, sine_spec.duration, sine_spec.wav_spec.sample_rate));
+        }
+        for i in 0..(sine_spec.duration as u32 * sine_spec.wav_spec.sample_rate) {
+            let mut freq_sum : f64 = 0.;
+            for freq in all_freqs_u8.iter() { freq_sum += freq[i as usize] as f64 }
+            data.push(freq_sum);
+            
+        }
+    }
+    
+    data
+}
+
+#[inline(always)]
+fn calculate_frequency(i: u32, freq: u16, sample_rate: u32) -> f64{
+    let x = (i as f64 / sample_rate as f64) * freq as f64 * 2. * PI;
+    x.sin()
+}
+
+fn write_frequency_8(freq: u16, duration: u16, sample_rate: u32) -> Vec<u8> {
     let mut data = vec![]; // data size will be sample rate * seconds
-    for i in 0..(sample_rate * time) {
+    for i in 0..(sample_rate * duration as u32) {
         let x = (i as f64 / sample_rate as f64) * freq as f64 * 2. * PI;
         data.push(match (x.sin() * 128.) as i16 + 127 {
             y if { y >= 0 } => y as u8,
@@ -32,3 +124,37 @@ fn write_frequency(freq: u32, sample_rate: usize, time: usize) -> Vec<u8> {
     }
     data
 }
+
+fn write_frequency_16(freq: u16, duration: u16, sample_rate: u32) -> Vec<u16> {
+    let mut data = vec![]; // data size will be sample rate * seconds
+    for i in 0..(sample_rate * duration as u32) {
+        let x = (i as f64 / sample_rate as f64) * freq as f64 * 2. * PI;
+        data.push(match (x.sin() * 32768.) as i16 + 32767 {
+            y if { y >= 0 } => y as u16,
+            y => {
+                println!("value was less than 0 at i = {}, y = {}", i, y);
+                0
+            },  
+        });
+    }
+    data
+}
+
+
+fn main() {
+
+    let spec = WavSpec::new(1, 44100, 8).expect("Invalid specification");
+    //let sinespec = ...
+    let mut writer = WavWriter::new_with_spec(spec, "test_8.wav").expect("Error creating file");
+    //write_wav(sinespec)...
+    let freqs: Vec<u16> = vec![440, 440 * 5 / 4]; // ein vektor aus frequenzen
+    let sine_spec = SineWavSpec::new(spec, freqs.to_vec(), 20, 0.).expect("invalid specification");
+
+    write_wave(sine_spec, writer);
+}
+
+// for data value in 0..sample_rate * duration
+// for frequency in frequencies
+// sum 
+// sum each frequency
+
